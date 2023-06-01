f6eec093ab4ec166afae2e3a1e254557e58da423
Ion Pascari <ionpascari1995@gmail.com>
2023-06-01 21:30:16 +0300

    feat: bootstrap the project

---
+++ src/main/java/inc/evil/cnsorderservice/CnsOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package inc.evil.cnsorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsOrderServiceApplication.class, args);
+	}
+
+}
